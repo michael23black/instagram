8e552f108c9413f57fe6f70ad7cf9f0b659b04d3
michael23black <michael23black@gmail.com>
2018-10-15 13:54:10 +0300

    -> Исправлено отоброжение значка в input в зависимости от количества введенного текста -> Удалены неиспользуемые таблицы стилей

--- bundles/project-instagram.js
+++ bundles/project-instagram.js
@@ -1,4 +1,9 @@
 $(function(){
+	$.fn.textWidth = function(text, font) {
+    	if (!$.fn.textWidth.fakeEl) $.fn.textWidth.fakeEl = $('<span>').hide().appendTo(document.body);
+    	$.fn.textWidth.fakeEl.text(text || this.val() || this.text()).css('font', font || this.css('font'));
+    	return $.fn.textWidth.fakeEl.width();
+	};
 	$(document).ready(function(){
 		$('.user-profile').after('<div class="trans-info"></div>');
 		$('.story-highlights').after('<div class="trans-subs"></div>');
@@ -31,11 +36,13 @@ $(function(){
 	$('.reset-button').on('mousedown', function () {
 		$('.input').val('');
 		$('.input').attr('placeholder', 'Поиск');
+		$('.input').css('background-position-x',"");
 		userText = false;
 	});
 	$('.input').on('keypress', function () {
 		if ($('.input').val().length >= 1){
 			$('.input').attr('placeholder', 'Поиск');
+			$('.input').css('background-position-x', "");
 			userText = false;
 		}
 	});
@@ -53,13 +60,21 @@ $(function(){
 		$('.input-button').css({'visibility':'collapse'});
 		$('.input').css({'background-image':'url(images/h-search.png)'});
 		if($('.input').val().length != 0){
-			$('.input').attr('placeholder', $("input").val());
-			userText = true;
+			if ($('.input').textWidth() <= 161) {
+				$('.input').attr('placeholder', $("input").val());
+				var margin = (($('.input').width())/ 2) - $('.input').textWidth() / 1.95 + 10;
+				$('.input').css('background-position-x',margin);
+				userText = true;
+			}
+			else {
+				$('.input').attr('placeholder', $("input").val());
+				$('.input').css('background-position-x','7px');
+				userText = true;
+			}
 		}
 		$('.input').val('');
 		$('.input').removeClass('placeholder-text-left');
 	});
-
 	$(window).on('resize', function(){
 		if ($(window).width() < 585){
 			$('.main-subs').appendTo('.trans-subs');
